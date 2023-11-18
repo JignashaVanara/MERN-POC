4d11762cabaedf00d4b888776dd7c3f0374d976d
JignashaVanara <vanarajignasha159p@gmail.com>
2023-11-17 22:34:07 -0500

    structure creation

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+var express = require('express');
+
+var app = express();
+
+//CORS middleware
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    next();
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    // res.redirect('/error').status(404);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
